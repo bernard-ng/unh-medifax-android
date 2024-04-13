4e6bdfff58de1fb21ab3c9d4887f78a6bec6f555
Eric Ampire <eric.ampire@brp.com>
2024-04-13 18:12:56 -0400

    Collect data

--- app/src/main/java/tech/devscast/medifax/MedifaxApplication.kt
+++ app/src/main/java/tech/devscast/medifax/MedifaxApplication.kt
@@ -2,7 +2,12 @@ package tech.devscast.medifax
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
+
 class MedifaxApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
--- app/src/main/java/tech/devscast/medifax/presentation/viewmodel/GetStartedViewModel.kt
+++ app/src/main/java/tech/devscast/medifax/presentation/viewmodel/GetStartedViewModel.kt
@@ -2,7 +2,8 @@ package tech.devscast.medifax.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class GetStartedViewModel : ViewModel() {
+class GetStartedViewModel @Inject constructor() : ViewModel() {
 }
