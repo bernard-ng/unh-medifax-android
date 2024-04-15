c3ccf507fda539660b90ea71d9830f8e349415a1
Othniel1508 <154688129+Othniel1508@users.noreply.github.com>
2024-04-15 21:34:17 +0200

    Logique SignUp

---
+++ app/src/main/java/tech/devscast/medifax/presentation/screens/sign/components/NotEmptyValidable.kt
@@ -0,0 +1,11 @@
+package tech.devscast.medifax.presentation.screens.sign.components
+
+class NotEmptyValidable : BaseValidable(validator = ::isFieldValid, errorFor = ::fieldValidationError)
+
+private fun isFieldValid(text: String): Boolean {
+    return text.isNotBlank()
+}
+
+private fun fieldValidationError(text: String): String {
+    return "This field is required"
+}
--- app/src/main/java/tech/devscast/medifax/presentation/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/tech/devscast/medifax/presentation/viewmodel/SignUpViewModel.kt
@@ -1,5 +1,7 @@
 package tech.devscast.medifax.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,14 +10,25 @@ import kotlinx.coroutines.launch
 import tech.devscast.medifax.domain.repository.PatientRepository
 import javax.inject.Inject
 
+sealed class RegistrationStatus{
+    object Success : RegistrationStatus()
+    object Error : RegistrationStatus()
+}
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
     private val repository: PatientRepository
 ): ViewModel() {
+    private val _registrationsStatus = MutableLiveData<RegistrationStatus>()
+    val registrationStatus: LiveData<RegistrationStatus> get() = _registrationsStatus
 
     fun register(email: String, password: String, fullName: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            repository.register(email, password, fullName)
+            val response = repository.register(email, password, fullName)
+            if (response.success){
+                _registrationsStatus.postValue(RegistrationStatus.Success)
+            } else {
+                _registrationsStatus.postValue(RegistrationStatus.Error)
+            }
         }
     }
 }
