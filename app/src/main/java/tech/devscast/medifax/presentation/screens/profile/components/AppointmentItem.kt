abe3b36bc3c7712974ef389f026c303e6823f34c
HenrietteK21 <henriengonga@gmail.com>
2024-04-16 23:41:05 +0200

    j'ai ajouter les deux pages onboarding 1 et 2 , puis j'ai effectué quelques modifications afin de faire passer le splashScreen

--- app/src/main/java/tech/devscast/medifax/presentation/screens/profile/components/AppointmentItem.kt
+++ app/src/main/java/tech/devscast/medifax/presentation/screens/profile/components/AppointmentItem.kt
@@ -4,10 +4,79 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import tech.devscast.medifax.data.entity.Appointment
 
+import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun convertStringToDate(dateString: String, format: String): Date {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    return dateFormat.parse(dateString) ?: Date()
+}
+
 @Composable
 fun AppointmentItem(appointment: Appointment) {
-    Text(appointment.doctor.fullName)
-    Text(appointment.date)
-    Text(appointment.description)
-    Text(appointment.status)
+    val date = convertStringToDate(appointment.date, "yyyy-MM-dd")
+    val formattedDate = DateFormat.getDateTimeInstance().format(date)
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .width(4.dp)
+                    .fillMaxHeight()
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Contenu du rendez-vous
+            Column {
+                Text(
+                    text = appointment.doctor.fullName,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = formattedDate,
+                    fontSize = 14.sp,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+                Text(
+                    text = appointment.description,
+                    fontSize = 14.sp,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+                Text(
+                    text = appointment.status,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+        }
+    }
 }
